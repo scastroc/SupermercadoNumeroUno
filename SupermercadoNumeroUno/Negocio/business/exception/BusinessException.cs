c00a812875f8408148b1da7c9314295ffed0c103
Sebastian Castro <sxdogma@hotmail.com>
2018-10-14 00:12:20 -0300

    Se agrega capa de negocio

---
+++ SupermercadoNumeroUno/Negocio/business/exception/BusinessException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.business.exception
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException() : base()
+        {
+        }
+
+        public BusinessException(string message)
+            : base(message)
+        {
+        }
+
+        public BusinessException(string message,
+            Exception ex) : base(message, ex)
+        {
+        }
+    }
+}
---
+++ SupermercadoNumeroUno/Negocio/business/rules/generic/EmptyRule.cs
@@ -0,0 +1,25 @@
+﻿using Negocio.business.exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.business.rules.generic
+{
+    public class EmptyRule
+    {
+        public void isEmpty(string nameElement,
+            string value)
+        {
+
+            if (string.IsNullOrEmpty(value))
+            {
+
+                throw new BusinessException(
+                    "EL CAMPO " + nameElement +
+                    " NO PUEDE ESTAR EN BLANCO");
+            }
+        }
+    }
+}
